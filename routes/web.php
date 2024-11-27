bdb63d2db72d64d2ec6a64b0911ea5e23a1f7331
minnatur-rahman <minnatur7373@gmail.com>
2024-11-27 21:50:12 +0600

    alhf

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('login', [AuthController::class, 'login']);
-Route::get('register', [AuthController::class, 'register']);
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::get('register', [AuthController::class, 'register'])->name("register");
