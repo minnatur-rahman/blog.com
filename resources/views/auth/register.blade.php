75ea9245afdbf6fcb6922f1ae5f41ff5fbac74f0
minnatur-rahman <minnatur7373@gmail.com>
2024-11-29 15:29:08 +0800

    lahfd

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -70,19 +70,19 @@
                     <div class="col-12">
                       <label for="yourName" class="form-label">Your Name</label>
                       <input type="text" name="name" value="{{ old('name') }}" class="form-control" id="yourName" required>
-                      <div class="invalid-feedback">Please, enter your name!</div>
+                      <div style="color: red">{{ $errors->first('name') }}</div>
                     </div>
 
                     <div class="col-12">
                       <label for="yourEmail" class="form-label">Your Email</label>
                       <input type="email" name="email" value="{{ old('email') }}" class="form-control" id="yourEmail" required>
-                      <div class="invalid-feedback">Please enter a valid Email adddress!</div>
+                      <div style="color: red">{{ $errors->first('email') }}</div>
                     </div>
 
                     <div class="col-12">
                       <label for="yourPassword" class="form-label">Password</label>
                       <input type="password" name="password" class="form-control" id="yourPassword" required>
-                      <div class="invalid-feedback">Please enter your password!</div>
+                      <div style="color: red">{{ $errors->first('password') }}</div>
                     </div>
 
                     <div class="col-12">
