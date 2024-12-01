ad1389d6f0b307db2bbf31e2ca8de5f9bc099ffb
minnatur-rahman <minnatur7373@gmail.com>
2024-12-01 20:16:35 +0600

    lashdf

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -1,15 +1,7 @@
 @component('mail::message')
-
-    Hi, {{ $user->name }}. Forgot Your Password?
-
-    <p>It happens. Click the link below to reset your password.</p>
-
     @component('mail::button', ['url' => url('reset/'.$user->remember_token)])
         Verify
     @endcomponent
-
-    
-
     {{ config('app.name') }}
 
 @endcomponent
