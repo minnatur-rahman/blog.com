10dc81a382ca3911f381c2883bb18acbe6e2ceb2
minnatur-rahman <minnatur7373@gmail.com>
2024-12-02 21:48:08 +0800

    ;saldfj

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -45,7 +45,13 @@ class AuthController extends Controller
           }
           else
           {
+            $save = new User;
+            $save-> remember_token = Str::random(40);        
+            $save->save();
 
+            Mail::to($save->email)->send(new RegisterMail($save));
+    
+            return redirect()->route('login')->with('success', 'Your Account Register Successfully and verify your email address.');    
           }
         }
         else
