fe42dd7d2da04b41a9fc531dac379727df7d748a
minnatur-rahman <minnatur7373@gmail.com>
2025-01-13 06:20:07 +0600

    lashf

--- resources/views/backend/user/add.blade.php
+++ resources/views/backend/user/add.blade.php
@@ -16,21 +16,24 @@
                 @csrf
               <div class="col-md-12">
                 <label for="inputName5" class="form-label">Name</label>
-                <input type="text" name="name" class="form-control" id="inputName5" required>
+                <input type="text" name="name" value="{{ old('name') }}" class="form-control" id="inputName5" required>
+                <div style="color:red">{{ $errors->first('name') }}</div>
               </div>
               <div class="col-md-12">
                 <label for="inputEmail5" class="form-label">Email</label>
-                <input type="email" name="email" class="form-control" id="inputEmail5" required>
+                <input type="email" name="email" value="{{ old('name') }}" class="form-control" id="inputEmail5" required>
+                <div style="color:red">{{ $errors->first('email') }}</div>
               </div>
               <div class="col-md-12">
                 <label for="inputPassword5" class="form-label">Password</label>
                 <input type="password" name="password" class="form-control" id="inputPassword5" required>
+                <div style="color:red">{{ $errors->first('password') }}</div>
               </div>
               <div class="col-md-12">
                 <label for="inputPassword5" class="form-label">Status</label>
                <select name="status" id="inputPassword5" class="form-control">
-                    <option value="1">Active</option>
-                    <option value="0">Inactive</option>
+                    <option {{ (old('status') == 1) ? 'selected' : '' }} value="1">Active</option>
+                    <option {{ (old('status') == 0) ? 'selected' : '' }} value="0">Inactive</option>
                </select>
               </div>
               <div class="col-md-12">
