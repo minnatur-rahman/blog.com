5f18aa5a0de9682ee809b75f45972c83237cd131
minnatur-rahman <minnatur7373@gmail.com>
2025-01-14 22:27:11 +0600

    ;sjf

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -4,42 +4,42 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item">
-        <a class="nav-link " href="{{ route('admin.dashboard') }}">
+        <a class="nav-link  @if(Request::segment(2) != 'dashboard') collapsed" href="{{ route('admin.dashboard') }}">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li><!-- End Dashboard Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ route('user.list') }} ">
+        <a class="nav-link  @if(Request::segment(2) != 'user') collapsed" href="{{ route('user.list') }} ">
           <i class="bi bi-person"></i>
           <span>Users</span>
         </a>
       </li><!-- End Profile Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('panel/category/list') }}">
+        <a class="nav-link  @if(Request::segment(2) != 'catetory') collapsed" href="{{ url('panel/category/list') }}">
           <i class="bi bi-person"></i>
           <span>Catetory</span>
         </a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('panel/blog/list') }}">
+        <a class="nav-link  @if(Request::segment(2) != 'blog') collapsed" href="{{ url('panel/blog/list') }}">
           <i class="bi bi-person"></i>
           <span>Blogs</span>
         </a>
       </li>
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="{{ url('panel/blog/help/list') }}">
+        <a class="nav-link  @if(Request::segment(2) != 'help') collapsed" href="{{ url('panel/blog/help/list') }}">
           <i class="bi bi-question-circle"></i>
           <span>Help</span>
         </a>
       </li><!-- End F.A.Q Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="pages-contact.html">
+        <a class="nav-link  @if(Request::segment(2) != 'inbox') collapsed" href="pages-contact.html">
           <i class="bi bi-envelope"></i>
           <span>Inbox</span>
         </a>
