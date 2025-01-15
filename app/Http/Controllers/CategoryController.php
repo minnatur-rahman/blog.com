8396239fc253f14cc409c0c60be5d88a6a7405ef
minnatur-rahman <minnatur7373@gmail.com>
2025-01-15 22:30:04 +0600

    ls

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function category()
     {
+        $data['getRecord'] = User::getRecordUser();
         $data['meta_title'] = 'Login';
         return view('backend.category.list',$data);
     }
--- resources/views/backend/category/list.blade.php
+++ resources/views/backend/category/list.blade.php
@@ -1,1 +1,64 @@
-ok
+@extends('backend.layouts.app')
+@section('style')
+@endsection
+@section('content')
+<section class="section">
+  <div class="row">
+    <div class="col-lg-12">
+      @include('layouts._message')
+      <div class="card">       
+        <div class="card-body">
+          <h5 class="card-title">
+            Category List
+            <a href="{{ url('panel/category/add') }}" class="btn btn-primary" style="float: right;margin-top: -12px;">Category Add</a>
+          </h5>
+
+          <!-- Table with stripped rows -->
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col">Emai Verified</th>
+                <th scope="col">Status</th>
+                <th scope="col">Created Date</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              @forelse ($getRecord as $value )
+                <tr>
+                  <th scope="row">{{ $value->id }}</th>
+                  <td>{{ $value->name }}</td>
+                  <td>{{ $value->email }}</td>
+                  <td>{{ !empty($value->email_verified_at) ? 'Yes' : 'No' }}</td>
+                  <td>{{ !empty($value->status) ? 'Active' : 'Inactive' }}</td>
+                  <td>{{ date('d-m-Y H:i A',strtotime($value->created_at)) }}</td>
+                  <td>
+                    <a href="{{ url('panel/user/edit/'.$value->id) }}" class="btn btn-primary btn-sm">Edit</a>
+                    <a onclick="return confirm('Are you sure you want to delete?');" href="{{ url('panel/user/delete/'.$value->id) }}" class="btn btn-danger btn-sm">Delete</a>
+                  </td>
+                </tr>
+              @empty
+                <tr>
+                  <td colspan="100%">Record not found</td>
+                </tr>
+              @endforelse
+
+            </tbody>
+          </table>
+          
+          {!! $getRecord->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+@endsection
+ 
+@section('script')
+@endsection
+
+ 
