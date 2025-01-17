dd162b543c58d453602ed004070a7f4a2b8f3986
minnatur-rahman <minnatur7373@gmail.com>
2025-01-17 20:31:03 +0600

    lsdjf

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -20,4 +20,18 @@ class CategoryController extends Controller
         $data['meta_title'] = 'Add Category';
         return view('backend.category.add',$data);
     }
+
+    public function store(Request $request)
+    {
+        $save = new Category;
+        $save->name = trim($request->name);
+        $save->title = trim($request->title);
+        $save->meta_title = trim($request->meta_title);
+        $save->meta_description = trim($request->meta_description);
+        $save->meta_keywords = trim($request->meta_keywords);
+        $save->status = trim($request->status);
+        $save->save();
+
+        return redirect('panel/category/list')->with('success', 'Data store successfully');
+    }
 }
