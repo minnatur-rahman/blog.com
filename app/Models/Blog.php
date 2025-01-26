9ce1971aac1acef1eed7a7b636bca77c8ea13191
minnatur-rahman <minnatur7373@gmail.com>
2025-01-26 21:47:05 +0600

    ;sfj

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -10,4 +10,13 @@ class Blog extends Model
     use HasFactory;
 
     protected $table = 'blogs';
+
+    static public function getRecord()
+    {
+        return self::select('blog.*', 'users.name as user_name', 'categories.name as category_name')
+                  ->join('users', 'users.id', '=', 'blog.user_id')
+                  ->join('categories', 'categories.id', '=', 'blog.category_id')
+                  ->orderBy('blog.id', 'desc')
+                  ->paginate(5);
+  }
 }
